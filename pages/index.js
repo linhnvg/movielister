2f3ad5a812ac1aac9a28bf0ebb12f099588ffb82
oktay <oktaycolakoglu@gmail.com>
2022-11-09 05:48:26 +0300

    Create homepage

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,85 @@
 import { tmdb } from '@lib/service'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Input, InputGroup, InputLabel } from '@components/input'
+import Card from '@components/card'
+import Navbar from '@components/navbar'
+import SegmentedControl from '@components/segmented-control'
+import Search from '@components/icons/search.svg'
+import Head from 'next/head'
+import Footer from '@components/footer'
 
 export default function Home({ data }) {
+  const router = useRouter()
+
   return (
     <div>
-      <nav>
-        <Link href="/" replace>
-          All
-        </Link>
-        <Link href="?tab=movie" replace>
-          Movies
-        </Link>
-        <Link href="?tab=tv" replace>
-          TV Shows
-        </Link>
-      </nav>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Head>
+        <title>Movielist - explore movies, tv shows and more</title>
+        <meta
+          name="description"
+          content="Movielist, Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis pariatur optio quas!"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+
+      <Navbar />
+
+      <div className="container pb-12">
+        <div className="my-20 max-w-xl">
+          <h1 className="heading-xl">Movielister</h1>
+          <p className="text-gray-400 mt-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            pariatur optio quas! ✌
+          </p>
+
+          <InputGroup className="mt-8">
+            <Input placeholder="eg. avengers" hasIcon />
+            <InputLabel>Search Movies or TV Shows</InputLabel>
+            <Search className="input-icon text-2xl" />
+          </InputGroup>
+        </div>
+
+        <div className="flex flex-col md:items-center md:flex-row">
+          <h2 className="heading text-gray-400 mr-6">Trendings</h2>
+
+          <SegmentedControl
+            className="my-6"
+            defaultIndex={0}
+            segments={[
+              {
+                label: 'All',
+                value: '',
+              },
+              {
+                label: 'Movies',
+                value: 'movie',
+              },
+              {
+                label: 'TV Shows',
+                value: 'tv',
+              },
+            ]}
+            callback={(val) =>
+              router.replace({ pathname: '/', query: { tab: val } })
+            }
+          />
+        </div>
+
+        <div className="card-list">
+          {data.results.map((result) => (
+            <Card
+              key={result.id}
+              id={result.id}
+              image={result.poster_path}
+              title={result.title || result.name}
+              type={result.media_type}
+              rating={result.vote_average}
+            />
+          ))}
+        </div>
+      </div>
+
+      <Footer />
     </div>
   )
 }
