aa87e2c780ac33b495a4fdf214487eb57c8a9ebc
oktay <oktaycolakoglu@gmail.com>
2022-11-09 15:44:31 +0300

    Render search results

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,102 @@
 import { tmdb } from '@lib/service'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import Navbar from '@components/navbar'
+import Footer from '@components/footer'
+import Search from '@components/search'
+import SearchIcon from '@components/icons/search.svg'
+import SegmentedControl from '@components/segmented-control'
+import Card from '@components/card'
+import Pagination from '@components/pagination'
 
-export default function Home({ data }) {
+export default function Home({ data, query }) {
+  const router = useRouter()
+
   return (
     <div className="animate-fade-in">
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Head>
+        <title>Search &mdash; Movielist</title>
+        <meta
+          name="description"
+          content="Movielist, Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis pariatur optio quas!"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+
+      <Navbar />
+
+      <div className="container pb-12">
+        <div className="my-20 max-w-xl">
+          <h1 className="heading-xl mt-4">Search</h1>
+          <Search />
+        </div>
+
+        {query.query && (
+          <div>
+            <div className="flex flex-col md:items-center md:flex-row">
+              <h2 className="heading text-gray-400 mr-6">Results</h2>
+
+              <SegmentedControl
+                className="my-6"
+                defaultIndex={
+                  query.type === 'collection' ? 2 : query.type === 'tv' ? 1 : 0
+                }
+                segments={[
+                  {
+                    label: 'Movies',
+                    value: '',
+                  },
+                  {
+                    label: 'TV Shows',
+                    value: 'tv',
+                  },
+                  {
+                    label: 'Collections',
+                    value: 'collection',
+                  },
+                ]}
+                callback={(val) =>
+                  router.replace({
+                    pathname: '/search',
+                    query: { ...router.query, type: val, page: 1 },
+                  })
+                }
+              />
+            </div>
+
+            {data.results?.length ? (
+              <div>
+                <div className="card-list">
+                  {data.results.map((result) => (
+                    <Card
+                      key={result.id}
+                      id={result.id}
+                      image={result.poster_path}
+                      title={result.title || result.name}
+                      type={query.type || 'movie'}
+                      rating={result.vote_average}
+                    />
+                  ))}
+                </div>
+                <Pagination
+                  totalPages={data.total_pages}
+                  currentPage={query.page}
+                  className="mt-8"
+                />
+              </div>
+            ) : (
+              <div className="h-96 grid place-items-center bg-black-10 rounded-xl">
+                <div className="flex flex-col items-center">
+                  <SearchIcon className="text-3xl mb-4" />
+                  <span>No result for {query.query}</span>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+
+      <Footer />
     </div>
   )
 }
@@ -14,7 +107,7 @@ export async function getServerSideProps({ query }) {
   if (!searchQuery) {
     return {
       props: {
-        data: {},
+        query,
       },
     }
   }
@@ -36,7 +129,8 @@ export async function getServerSideProps({ query }) {
       props: {
         error: {
           statusCode: response.status,
-          statusMessage: response.data.status_message,
+          statusMessage:
+            response.data.errors[0] || response.data.status.message,
         },
       },
     }
@@ -45,6 +139,7 @@ export async function getServerSideProps({ query }) {
   return {
     props: {
       data: response.data,
+      query,
     },
   }
 }
