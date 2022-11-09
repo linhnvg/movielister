b7845265d1bf0a63a84cdfd5b36a672c2bc8df3d
oktay <oktaycolakoglu@gmail.com>
2022-11-09 05:14:46 +0300

    Add rating to card component

--- components/card.js
+++ components/card.js
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import Poster from './poster'
 import clsx from 'clsx'
+import Rating from './rating'
 
 export default function Card({
   id,
   image,
   title,
   type,
+  rating,
   className,
   children,
   ...props
@@ -17,9 +19,10 @@ export default function Card({
       className={clsx('card', className)}
       {...props}
     >
-      <Poster path={image} alt={title} />
-      <div className="card-title">{title}</div>
-      <div>{children}</div>
+      {image && <Poster path={image} alt={title} />}
+      {rating && <Rating average={rating} className="absolute top-4 left-4" />}
+      {title && <div className="card-title">{title}</div>}
+      {children}
     </Link>
   )
 }
