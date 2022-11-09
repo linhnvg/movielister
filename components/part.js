c848b077d7f44cd01ac3bb05d3ff2468258f98d7
oktay <oktaycolakoglu@gmail.com>
2022-11-10 01:28:27 +0300

    Create part component

---
+++ components/part.js
@@ -0,0 +1,39 @@
+import { format } from 'date-fns'
+import Link from 'next/link'
+import Card from './card'
+import ArrowIcon from './icons/arrow.svg'
+
+export default function Part({ id, poster, rating, date, title, overview }) {
+  return (
+    <div
+      key={id}
+      className="flex flex-col md:flex-row bg-black-30 p-8 gap-8 rounded-xl"
+    >
+      {poster && (
+        <div className="w-full md:w-1/3  lg:w-1/5 flex-shrink-0">
+          <Card
+            id={id}
+            image={poster}
+            type="movie"
+            rating={rating}
+            className="pb-2"
+          />
+        </div>
+      )}
+      <div className="w-full">
+        {date && (
+          <span className="block">{format(new Date(date), 'yyyy')}</span>
+        )}
+        {title && <strong className="heading">{title}</strong>}
+        {overview && <p className="text-white-65 mt-2">{overview}</p>}
+        <Link
+          href={`/movie/${id}`}
+          className="button button-primary button-sm mt-4"
+        >
+          Details
+          <ArrowIcon className="text-xl ml-4" />
+        </Link>
+      </div>
+    </div>
+  )
+}
