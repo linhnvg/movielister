dfa9e3ff8d5a08b6336bed528e45fd9aa059212a
oktay <oktaycolakoglu@gmail.com>
2022-11-11 02:55:38 +0300

    Fix breadcrumb break

--- components/breadcrumb.js
+++ components/breadcrumb.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Breadcrumb({ pages }) {
   return (
-    <nav className="flex items-center text-sm">
+    <nav className="flex items-center text-sm flex-wrap gap-1">
       {pages.map(({ href, label }) => (
         <Link key={href} href={href} className="breadcrumb">
           {label}
