137073edab46dfbb47e1b0b344ba5b6e91a26008
oktay <oktaycolakoglu@gmail.com>
2022-11-11 23:45:03 +0300

    Add animation to modal

--- components/modal.js
+++ components/modal.js
@@ -1,19 +1,31 @@
 import { createPortal } from 'react-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function Modal({ children, isOpen, setIsOpen }) {
   if (typeof window === 'object')
     return createPortal(
-      <>
+      <AnimatePresence>
         {isOpen && (
-          <div className="fixed inset-0 grid place-items-center z-50">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 grid place-items-center z-50"
+          >
             <div
               className="fixed inset-0 bg-black-65"
               onClick={() => setIsOpen(false)}
             />
-            <div className="relative">{children}</div>
-          </div>
+            <motion.div
+              className="relative"
+              initial={{ translateY: 10 }}
+              animate={{ translateY: 0 }}
+            >
+              {children}
+            </motion.div>
+          </motion.div>
         )}
-      </>,
+      </AnimatePresence>,
       document.querySelector('#__next')
     )
 }
