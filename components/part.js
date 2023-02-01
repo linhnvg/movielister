d3ce8e35e45ed387fddd3e6d163fb0e90b8306e9
oktay <oktaycolakoglu@gmail.com>
2023-02-01 16:09:18 +0300

    Remove poster check from collection part

--- components/part.js
+++ components/part.js
@@ -9,22 +9,20 @@ export default function Part({ id, poster, rating, date, title, overview }) {
       key={id}
       className="flex flex-col md:flex-row bg-black-30 p-8 gap-8 rounded-xl"
     >
-      {poster && (
-        <div className="w-full md:w-1/3  lg:w-1/5 flex-shrink-0">
-          <Card
-            id={id}
-            image={poster}
-            type="movie"
-            rating={rating}
-            className="pb-2"
-          />
-        </div>
-      )}
+      <div className="w-full md:w-1/3  lg:w-1/5 flex-shrink-0">
+        <Card
+          id={id}
+          image={poster}
+          type="movie"
+          rating={rating}
+          className="pb-2"
+        />
+      </div>
       <div className="w-full">
         {date && (
           <span className="block">{format(new Date(date), 'yyyy')}</span>
         )}
-        {title && <strong className="heading">{title}</strong>}
+        {title && <strong className="heading block">{title}</strong>}
         {overview && <p className="text-white-65 mt-2">{overview}</p>}
         <Link
           href={`/movie/${id}`}
