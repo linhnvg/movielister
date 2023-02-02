7578593e7abc17ffaa4fe44f7378ab445bb09f95
oktay <oktaycolakoglu@gmail.com>
2023-02-02 18:44:33 +0300

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import '../styles/globals.css'
 import '@fontsource/poppins/400.css'
 import '@fontsource/poppins/600.css'
 import '@fontsource/poppins/700.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   if (pageProps.error)
@@ -16,6 +17,20 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-YRMKVQ13FX"
+      />
+      <Script strategy="afterInteractive" id="ga">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-YRMKVQ13FX');
+        `}
+      </Script>
+
       <NextNProgress color="#5A4AF4" />
       <Component {...pageProps} />
     </>
