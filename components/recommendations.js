190ab56d8b5cdfb30c25fd152947fba91320d9d6
oktay <oktaycolakoglu@gmail.com>
2023-02-12 19:51:51 +0300

    Change recommendation cards

--- components/recommendations.js
+++ components/recommendations.js
@@ -20,6 +20,7 @@ export default function Recommendations({ recommendations }) {
           <a
             href={`/${result.title ? 'movie' : 'tv'}/${result.id}`}
             key={result.id}
+            className="relative"
           >
             <div className="aspect-backdrop h-44">
               <img
@@ -32,9 +33,12 @@ export default function Recommendations({ recommendations }) {
                 alt={result.title || result.name}
               />
             </div>
-            <div className="flex mt-2 items-center">
-              <Rating average={result.vote_average} />
-              <span className="text-sm ml-2">
+            <div className="bg-black-40 flex mt-2 items-center rounded-lg">
+              <Rating
+                average={result.vote_average}
+                className="absolute top-2 left-2"
+              />
+              <span className="text-md absolute bottom-4 left-2 bg-black-50 p-2 rounded-lg">
                 {result.title || result.name}
               </span>
             </div>
